2f28b02e107825fd7710f48b43ba605f977cbd67
majditounsi-spec <majdi.tounsi@gmail.com>
2026-04-03 20:57:00 +0200

    Update base path to /hemsida/ after repo rename

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/agency360-clone/",
+  base: "/hemsida/",
   server: {
     host: "::",
     port: 8080,
